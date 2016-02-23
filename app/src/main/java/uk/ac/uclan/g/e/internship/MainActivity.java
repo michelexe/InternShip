e30339866d9723f8f4f80fae34a275c9dc584253
michelexe <enzo.fabre@orange.fr>
2016-02-23 12:33:43 +0000

    Bluetooth -Activation -Notification d'erreur -Liste des périphériques connus

--- app/src/main/java/uk/ac/uclan/g/e/internship/MainActivity.java
+++ app/src/main/java/uk/ac/uclan/g/e/internship/MainActivity.java
@@ -1,47 +1,46 @@
 package uk.ac.uclan.g.e.internship;
 
-import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    private static boolean soundEnable = true;
+    private Notification notificationBluetooth;
+    private NotificationManager notifManager;
     private BluetoothAdapter bluetoothAdapter;
     private static final int REQUEST_ENABLE_BT=0;
     private Set<BluetoothDevice> devices; // this will contain the list of bluetooth device
+    private ImageButton button_sound;
 
     //  http://www.tutos-android.com/utilisation-bluetooth-application-android
+    // animation: http://developer.android.com/guide/topics/graphics/prop-animation.html
+    // https://www.youtube.com/watch?v=uP9Bbzk_sB0
+    // http://stackoverflow.com/questions/1520887/how-to-pause-sleep-thread-or-process-in-android
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.init();
         this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        // Bluetooth application  != if bluetooth is activated
+        // Bluetooth application
+        // != nul if bluetooth is available on this device
 
-        final Button playButton = (Button) findViewById(R.id.playButton);
 
-        final Notification notificationBluetooth =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.warning_icone)
-                        .setContentTitle("Error")
-                        .setContentText("Bluetooth isn't available on this device").build();
-            // the workseet method didn't work
-        //
 
-        final NotificationManager notifManager =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         if(bluetoothAdapter == null){
             notifManager.notify(1, notificationBluetooth);
@@ -51,20 +50,124 @@ public class MainActivity extends AppCompatActivity {
         }else{
             if (!bluetoothAdapter.isEnabled()) { // if Bluetooth isn't activate yet
                 bluetoothAdapter.enable(); // automatic activation of Bluetooth
+                devices = bluetoothAdapter.getBondedDevices();
                 Toast.makeText(getApplicationContext(),"Bluetooth activated",
                         Toast.LENGTH_LONG).show();
             }else{
                 devices = bluetoothAdapter.getBondedDevices();
-                // all known device
+                // all devices known
                 for (BluetoothDevice blueDevice : devices) {
                     Toast.makeText(getApplicationContext(), "Device = " + blueDevice.getName(),
                             Toast.LENGTH_SHORT).show();
                 }
             }
-
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_ENABLE_BT)
+            return; // end of method
+        if (resultCode == RESULT_OK) {
+            Toast.makeText(getApplicationContext(),"Bluetooth is activated",
+                    Toast.LENGTH_LONG).show();
+            bluetoothSeekEnable(true);
+            // L'utilisation a activé le bluetooth
+        } else {
+            Toast.makeText(getApplicationContext(),"Bluetooth is required to play",
+                    Toast.LENGTH_LONG).show();
+            // L'utilisation n'a pas activé le bluetooth
+        }
+    }
+
+    /**
+     * give the state of button_sound
+     * @return soundEnable
+     */
+    public static boolean getStateSound(){
+        return MainActivity.soundEnable;
+    }
+
+    /**
+     * to enable or disable button_sound
+     * if sate == false button_sound is disable
+     * @param state
+     */
+    public static void setSoundState(boolean state){
+        MainActivity.soundEnable=state;
+    }
+
+    /**
+     * initialisation of notification system
+     * there is only one notification for now
+     * which appears if bluetooth isn't available on the device
+     */
+    private void setNotification (){
+        notificationBluetooth =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.warning_icone)
+                        .setContentTitle("Error")
+                        .setContentText("Bluetooth isn't available on this device").build();
+        // the workseet method didn't work
+        //
+
+        notifManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+    }
+
+    /**
+     * uses several other function
+     */
+    private void init(){ // initialisation function
+        this.setNotification(); // implementation of notification
+        this.addClikListener(); // implementation of Listener on buttons
+        this.bluetoothSeekEnable(true);
+        this.bluetoothManagement(); // implementation of bluetooth
+    }
+
+    private void autoConnect(){
+        // connection to the band
+    }
 
 
+    private void bluetoothManagement(){ // initialisation of bluetooth
+        // empty
+
+
+    }
+
+    private void bluetoothSeekEnable(boolean state){
+        Intent discoverableIntent;
+        if(state){
+            discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+
+        }else{
+            discoverableIntent = new Intent(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        }
+        startActivity(discoverableIntent);
+    }
+    private void addClikListener(){
+        View.OnClickListener imgButtonHandler = new View.OnClickListener() {
+            // create a View.Onclick Listener which will be use by button_sound
+            public void onClick(View v) {
+                if(soundEnable){ // if the sound is enable
+                    MainActivity.setSoundState(false); // disable sound
+                    button_sound.setImageResource(R.drawable.icone_sound_mute);
+                    // load image which coresponds at sound_inactive
+                }else {
+                    MainActivity.setSoundState(true);
+                    button_sound.setImageResource(R.drawable.sound_active);
+                }
+            }
+        };
+
+        this.button_sound = (ImageButton) findViewById(R.id.button_sound_active);
+        this.button_sound.setOnClickListener(imgButtonHandler);
+
+        final Button playButton = (Button) findViewById(R.id.playButton);
         playButton.setOnClickListener(new View.OnClickListener() { // Add action
             @Override
             public void onClick(View v) {
@@ -77,10 +180,12 @@ public class MainActivity extends AppCompatActivity {
                     if (!bluetoothAdapter.isEnabled()) {
                         Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                         startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                            /* dialog which ask for the permission to use Bluetooth
+                            /* dialog which ask the permission to use Bluetooth
                             the answer will be treated by the function onActivityResult
                              */
 
+                    }else{
+                        bluetoothSeekEnable(false);
                     }
                     /*
                     Intent nextActivity = new Intent(getApplicationContext(), //nextActity);
@@ -89,31 +194,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != REQUEST_ENABLE_BT)
-            return; // end of method
-        if (resultCode == RESULT_OK) {
-            Toast.makeText(getApplicationContext(),"Bluetooth is activated",
-                    Toast.LENGTH_LONG).show();
-            // L'utilisation a activé le bluetooth
-        } else {
-            Toast.makeText(getApplicationContext(),"Bluetooth is required to play",
-                    Toast.LENGTH_LONG).show();
-            // L'utilisation n'a pas activé le bluetooth
-        }
     }
 
 
-    private void autoConnect(){
-        // connection to the band
-    }
 }
 
 
